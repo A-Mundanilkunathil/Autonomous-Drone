2c6a33cba15e5860c602331bf25b19683c05244e
nmhp16 <nmhieu.pham@gmail.com>
2025-08-19 14:08:00 -0700

    Update camera configuration: change xclk frequency to 20MHz, frame size to SVGA, increase JPEG quality compression to 25, and reduce framebuffer count to 1

--- system/drone_vision/esp32_udp.ino
+++ system/drone_vision/esp32_udp.ino
@@ -80,11 +80,11 @@ void setup() {
   config.pin_sscb_scl = SIOC_GPIO_NUM;
   config.pin_pwdn = PWDN_GPIO_NUM;
   config.pin_reset = RESET_GPIO_NUM;
-  config.xclk_freq_hz = 6000000;
+  config.xclk_freq_hz = 20000000;
   config.pixel_format = PIXFORMAT_JPEG;
-  config.frame_size = FRAMESIZE_VGA;
-  config.jpeg_quality = 20;
-  config.fb_count = 2;
+  config.frame_size = FRAMESIZE_SVGA;
+  config.jpeg_quality = 25;
+  config.fb_count = 1;
   config.fb_location = CAMERA_FB_IN_PSRAM;
   config.grab_mode = CAMERA_GRAB_LATEST;
 
@@ -102,7 +102,6 @@ void loop() {
   
   camera_fb_t * fb = esp_camera_fb_get();
   if (!fb) {
-    delay(1); 
     return;
   }
 
