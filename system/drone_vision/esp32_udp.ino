038f710dea6ebf218ec63a74d436946dd1f4b52f
nmhp16 <nmhieu.pham@gmail.com>
2025-08-19 12:44:02 -0700

    Adjust camera configuration: change xclk frequency to 6MHz and increase JPEG quality to 20

--- system/drone_vision/esp32_udp.ino
+++ system/drone_vision/esp32_udp.ino
@@ -80,10 +80,10 @@ void setup() {
   config.pin_sscb_scl = SIOC_GPIO_NUM;
   config.pin_pwdn = PWDN_GPIO_NUM;
   config.pin_reset = RESET_GPIO_NUM;
-  config.xclk_freq_hz = 10000000;
+  config.xclk_freq_hz = 6000000;
   config.pixel_format = PIXFORMAT_JPEG;
   config.frame_size = FRAMESIZE_VGA;
-  config.jpeg_quality = 10;
+  config.jpeg_quality = 20;
   config.fb_count = 2;
   config.fb_location = CAMERA_FB_IN_PSRAM;
   config.grab_mode = CAMERA_GRAB_LATEST;
